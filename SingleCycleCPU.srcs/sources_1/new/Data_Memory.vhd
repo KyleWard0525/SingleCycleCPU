a9a111269bdad82115020e0d99d9eee0fed7c7df
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-05-04 15:58:24 -0400

    Fixed error with loading values from memory

--- SingleCycleCPU.srcs/sources_1/new/Data_Memory.vhd
+++ SingleCycleCPU.srcs/sources_1/new/Data_Memory.vhd
@@ -39,20 +39,33 @@ entity Data_Memory is
 end Data_Memory;
 
 architecture rtl of Data_Memory is
-    type memory is array (63 downto 0) of std_logic_vector(7 downto 0);
+    type memory is array (999999 downto 0) of std_logic_vector(7 downto 0);
+    
+    
+     --  std_logic_vector to string
+    function slv_to_string ( a: std_logic_vector) return string is
+        variable b : string (a'length downto 1) := (others => NUL);
+    begin
+        for i in a'length downto 1 loop
+        b(i) := std_logic'image(a((i-1)))(2);
+        end loop;
+    return b;
+    end function;
+    
+    
     
     --  Assign memory signal and initialize the first 8 bytes
     signal mem_locs : memory := (
         
-        "00000000", --  byte 0
-        "00000000", --  byte 1
-        "00000000", --  byte 2
-        "00000000", --  byte 3
-        "00000000", --  byte 4
-        "00000000", --  byte 5
-        "00000000", --  byte 6
-        "00000000", --  byte 7
-        others => "00000000" -- Initialize the remaining locations to all zeros
+        "10000000", --  byte 0
+        "01000000", --  byte 1
+        "00100000", --  byte 2
+        "00010000", --  byte 3
+        "00001000", --  byte 4
+        "00000100", --  byte 5
+        "00000010", --  byte 6
+        "00000001", --  byte 7
+        others => "00110101" -- Initialize the remaining locations to all zeros
     );
     
     
@@ -62,21 +75,32 @@ begin
         variable low : integer := 0;
         variable high : integer := 7;
         variable bytes : integer := 7;
+        variable int : integer;
     begin
+        
+        
         
         -- Check if instruction is read or write
         if mem_write = '1' then
+            
+            low := 0;
+            high := 7;
+
             
             --Write first byte to memory
             mem_locs(to_integer(unsigned(input_addr))) <= input_data(high downto low);
             
             for i in 1 to bytes loop
-              --Increment byte
+              
+               --Increment byte
               low := low + 8;
               high := high + 8;
+             
+              
               -- Write next bytes
               mem_locs(to_integer(unsigned(input_addr)) + i) <= input_data(high downto low);
               
+             
             end loop;
    
             
@@ -84,6 +108,9 @@ begin
         else if mem_read = '1' then
             low := 0;
             high := 7;
+            
+            report "Input address in mem_read = " & slv_to_string(input_addr);
+            
             --Read first byte from memory
             mem_data(high downto low) <= mem_locs(to_integer(unsigned(input_addr)));
             
@@ -92,12 +119,16 @@ begin
                 low := low + 8;
                 high := high + 8;
                 
+                
                 --Read next bytes
                 mem_data(high downto low) <= mem_locs(to_integer(unsigned(input_addr) + i));
             
             end loop;
     end if;   
     end if;
+    
+    --low := 0;
+   -- high := 0;
     
     wait on input_addr, input_data, mem_write, mem_read; -- Wait for change in input signals
     
