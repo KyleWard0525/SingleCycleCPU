352c88536da184b887441b5a96dba109a145acc8
Kyle Ward <36857534+KyleWard0525@users.noreply.github.com>
2021-05-04 14:20:44 -0400

    Added multiplexor to select correct immediate field from the instruction

--- SingleCycleCPU.srcs/sources_1/new/Sign_Extend.vhd
+++ SingleCycleCPU.srcs/sources_1/new/Sign_Extend.vhd
@@ -31,18 +31,48 @@ use IEEE.NUMERIC_STD.ALL;
 entity Sign_Extend is
 port (
         input : in std_logic_vector(31 downto 0);
-        output : out std_logic_vector(63 downto 0)
+        output : inout std_logic_vector(63 downto 0)
     );
 end Sign_Extend;
 
 architecture rtl of Sign_Extend is
-
+     --  std_logic_vector to string
+    function slv_to_string ( a: std_logic_vector) return string is
+        variable b : string (a'length-1 downto 1) := (others => NUL);
+    begin
+        for i in a'length-1 downto 1 loop
+        b(i) := std_logic'image(a((i-1)))(2);
+        end loop;
+    return b;
+    end function;
 begin
 
     process is
+        variable dtiOffset : std_logic_vector(8 downto 0); --   Instruction offset for data transfer
+        variable branchOffset : std_logic_vector(18 downto 0); --   Intstruction offset for branch 
+    
     begin
-        -- Pad output with MSB
-        output <= std_logic_vector(resize(signed(input), output'length));
+        --  Determine if instruction is data transfer or branch
+        if input(26) = '1' then
+            --  Branch
+            branchOffset := input(23 downto 5);
+        
+            -- Sign extend
+            output <= std_logic_vector(resize(signed(branchOffset), output'length));
+            
+        --  Data transfer instruction
+        else if input(26) = '0' then
+            
+            dtiOffset := input(20 downto 12);
+            
+            -- Sign extend
+            output <= std_logic_vector(resize(signed(branchOffset), output'length));
+           
+        
+        end if;    
+       end if;
+    
+        
         wait on input;
     end process;
 
